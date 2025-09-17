f5e1fe826b38ff093c1c63443aa28968e1f85ac8
Hannie <stabili.fernandes@escola.pr.gov.br>
2025-09-17 15:02:23 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,3 +2,15 @@ $(".theme-switch").on("click", () => {
     $("body").toggleClass
     ("light-theme");
 });
+
+
+
+
+
+$(document).ready(function(){
+    $('#autoWidth').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function(){ $('#autoWidth').removeClass('cS-hidden');}
+    });
+});
